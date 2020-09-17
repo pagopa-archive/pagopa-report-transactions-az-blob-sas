21f615e3591dcb3869adc0fa8d9e763a6ed07216
pasqualespica <pasqualespica@gmail.com>
2020-09-17 11:43:56 +0200

    init repo and terrafrom state on azure

---
+++ infrastructure/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">=0.12.20"
+  backend "azurerm" {
+    resource_group_name  = "rg-pagopa-psp-report-transaction-terraform"
+    storage_account_name = "stpspreportstate"
+    container_name       = "tstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  version = "~> 1.42.0"
+}
+
+# Create a new Resource Group
+resource "azurerm_resource_group" "resource_group" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = var.tags
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,17 @@
+variable "tags" {
+  type = map
+  default = {
+    environment = "Production"
+    terraform   = "True"
+  }
+}
+
+variable "location" {
+  type    = string
+  default = "westeurope"
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "rg-pagopa-psp-report-transaction"
+}
